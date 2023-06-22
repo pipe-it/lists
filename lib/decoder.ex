25418148ebd90e9f37944573ba27af35cef751b3
sandeeperanti <sandeep@gmx.ru>
2023-06-22 11:31:24 +0530

    hotel rates 2

--- lib/decoder.ex
+++ lib/decoder.ex
@@ -13,27 +13,12 @@ defmodule Decoder do
     list
   end
 
-  def get_hotel_ids(list) do
-    Enum.map(list, fn hotel ->
-      get_hotel_id(hotel)
-    end)
-  end
-
-  def get_hotel_id(hotel_details) do
-    %{"HotelID" => id} = hotel_details
-    id
-  end
-
   def get_rate_plan_lists(list) do
     Enum.map(list, fn hotel ->
-      sortlist = get_rate_plan_list(hotel)
-      [h | _t] = Enum.sort_by(sortlist, & &1["TotalPrice"], :asc)
-      Map.put(h, "HotelID", get_hotel_id(hotel))
+      %{"RatePlanList" => ratelist} = hotel
+      [h | _t] = Enum.sort_by(ratelist, & &1["TotalPrice"], :asc)
+      %{"HotelID" => id} = hotel
+      Map.put(h, "HotelID", id)
     end)
-  end
-
-  def get_rate_plan_list(hotel_details) do
-    %{"RatePlanList" => ratelist} = hotel_details
-    ratelist
   end
 end
