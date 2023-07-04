b50616e8c084514e2f0ff8544a4c0c34db3a0bb3
sandeeperanti <sandeep@gmx.ru>
2023-07-04 16:33:09 +0530

    none

--- lib/datar.ex
+++ lib/datar.ex
@@ -1,14 +1,13 @@
-defmodule Data do
+defmodule Datar do
   def read(path) do
     file_data = File.read(path)
 
     case file_data do
       {:error, reason} -> {:error, reason}
-      {:ok, binary} -> Jason.decode!(binary)
+      {:ok, binary} -> Jason.decode!(binary) 
     end
   end
 
-  # jhjaswgdsa
   def get_doc_count(data) do
     result =
       Map.get(data, "aggregations")
@@ -17,12 +16,17 @@ defmodule Data do
       |> Enum.map(fn %{"key" => key, "4" => four} ->
         four
         |> Map.get("buckets")
-        |> Enum.map(fn %{"key" => ep, "5" => five} ->
-          case Map.get(five, "buckets") do
+        # |> Enum.map(fn %{"key" => ep, "5" => five} ->
+        #   case Map.get(five, "buckets") do
+          
+
+          |> Enum.reduce(fn ->
+
             [
               %{"key_as_string" => key1, "doc_count" => key1_count},
               %{"key_as_string" => key2, "doc_count" => key2_count}
-            ] ->
+            ],
+            
               %{
                 "ep" => ep,
                 key1 => key1_count,
@@ -39,12 +43,18 @@ defmodule Data do
               %{"key_as_string" => key = "false", "doc_count" => key_count}
             ] ->
               %{"ep" => ep, key => key_count, "false" => 0, "date" => "2023-06-28 00:00:00"}
-          end
-        end)
+       
+          end)      
+
+
+        #   end 
+
+        # end)
         |> Enum.map(&Map.put(&1, "key", key))
       end)
       |> List.flatten()
-      |> Jason.encode!()
+
+    # |> Jason.encode!()
 
     # File.write!("assets/result.json", result)
   end
